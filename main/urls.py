6bc5a60ffcf17514ff10516d7169ee583784088a
luuvmane <mrgildnow@gmail.com>
2024-11-13 17:14:30 +0300

    added contacts and about

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from . import views
 from main.apps import MainConfig
 from main.views import IndexView
 
@@ -7,5 +7,7 @@ app_name = MainConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('contacts/', views.contacts_view, name='contacts'),
+    path('about/', views.about_view, name='about'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,36 @@
 from django.views.generic import TemplateView
-
 from main.utils import get_random_choice
 from services.models import Service
 from speciality.models import Speciality
 from users.models import Doctor
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def contacts_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # Отправка письма
+        send_mail(
+            f'Сообщение от {name}',
+            message,
+            email,
+            [settings.DEFAULT_FROM_EMAIL],
+            fail_silently=False,
+        )
+        return HttpResponseRedirect(reverse('main:contacts'))
+
+    return render(request, 'main/contacts.html')
+
+
+def about_view(request):
+    return render(request, 'main/about.html')
 
 
 class IndexView(TemplateView):
